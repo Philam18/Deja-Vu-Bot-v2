348fa47e04b78696c12bf085e8405b88fba3cd1d
Philam18 <philiplam818@gmail.com>
2018-02-27 17:28:39 -0500

    Added functionality for queueing

--- MusicPlayer.js
+++ MusicPlayer.js
@@ -6,18 +6,29 @@ const GREEN = 0x00c939;
 const YELLOW = 0xe5c300;
 // Stream reading options
 const YOUTUBE_OPTIONS = {quality: 'highestaudio',filter: 'audioonly'};
-const STREAM_OPTIONS = {seek : 0, volume : 0.5, bitrate: 'auto'};
+const STREAM_OPTIONS = {seek : 0, volume : 0.1, bitrate: 'auto'};
 
 class MusicPlayer{
-  constructor(){
+  constructor(client){
+    this.client = client;
     this.connection = null;
     this.musicQueue = [];
     this.nowPlaying = {};
+    this.textChannel = {};
+
+    for (let guild of client.guilds){
+      console.log(";) " + guild.id);
+      this.musicQueue.push({
+        id:guild.id,
+        queue:[]
+      });
+    }
+
   }
 
   command(message){
+    this.textChannel = message.channel;
     var voiceChannel = message.member.voiceChannel;
-    var textChannel = message.channel;
     //Embed response template
     var response = new Discord.RichEmbed()
     .setTimestamp(message.createdAt)
@@ -25,42 +36,33 @@ class MusicPlayer{
     //-----------------------------------------------------------------------
     if(message.content === '!stop'){
       if(this.connection != null){
-        console.log("[MB] Stopped bot");
+        console.log("[MusicPlayer] Stopped bot");
+        console.log('-------------------------------------------------------------------------');
         response.setColor(GREEN);
         response.setDescription(`Stopped`);
         this.disconnectVoiceChannel();
-        textChannel.send(response);
+        this.textChannel.send(response);
         message.delete();
       }
     }
     //-----------------------------------------------------------------------
     if(/^(\!play )/i.exec(message.content)){
       if(voiceChannel == null){
-        console.log(`${message.author.username} is not in a voice channel`);
+        console.log(`[MusicPlayer] user not in voice channel: ${message.author.username}`);
+        console.log('-------------------------------------------------------------------------');
         response.setColor(RED);
         response.setDescription(`**${message.author.username}** is not in a voice channel.`);
-        textChannel.send(response);
+        this.textChannel.send(response);
         message.delete();
         return;
       }
       var url = message.content.replace(/^(\!play )/i,'');
-      console.log(`[MB] Connected to voice channel: ${voiceChannel.name}`);
-      this.connectToVoiceChannel(voiceChannel,(connection)=>{
-        /////////////////////////////////////////////////////////
-        connection.on('failed', (error)=>{
-          console.log("[MB.connection] Failed: " + error.message);
-        })
-        connection.on('warn',(string)=>{
-          console.log("[MB.connection] Warning: " + string);
-        });
-        connection.on("error", (error)=>{
-          console.log("[MB.connection] Error: " + error.message);
-        });
-        this.play(url,response,(res)=>{
-          textChannel.send(res);
+      console.log(`[MusicPlayer] Connected to voice channel: ${voiceChannel.name}`);
+      this.connectToVoiceChannel(voiceChannel,()=>{
+        this.play(url,response,(responseBack)=>{
+          this.textChannel.send(responseBack);
           message.delete();
         });
-        /////////////////////////////////////////////////////////
       });
       return;
     }
@@ -70,14 +72,15 @@ class MusicPlayer{
   }
 
   connectToVoiceChannel(voiceChannel, callback){
-    voiceChannel.join().then(connection=>{
+    voiceChannel.join().then((connection)=>{
       this.connection = connection;
-      if(callback) callback(connection);
+      if(callback) callback();
     });
   }
 
   disconnectVoiceChannel(){
     this.connection.channel.leave();
+    this.broadcast = null;
     this.connection = null;
   }
 
@@ -86,63 +89,89 @@ class MusicPlayer{
     return null;
   }
 
-  play(url,response,callback){
+  play(url, response, callback){
     //Attempt to grab song info at URL
     ytdl.getInfo(url,(error, info)=>{
       //First verify if the URL is valid
       if(error){
-        console.log("[MB] ytdl error: " + error.message);
+        console.log("[MusicPlayer] ytdl error: " + error.message);
+        console.log('-------------------------------------------------------------------------');
         response.setColor(RED);
         response.setTitle(error.message);
-        callback(response);
+        if(callback) callback(response);
         return;
       }
-      //Create object of song
+      //Create object of song with information about title, artist, thumbnail
       var song = {
         title:info.title,
         artist:info.author.name,
         thumbnail:info.thumbnail_url,
         url: url
       }
-      //Check if the
+      Check if the song is already in the playlist
       for(let i = 0; i < this.musicQueue.length;i++){
         var item = this.musicQueue[i];
+        //We cant use URL to check here because of stringQueries in URLs
+        //(eg. time seek, playlists, etc, inside the URL)
+        //So instead we simply check title and artist
         if(item.title == song.title && item.artist == song.artist){
-          console.log("[MB] Cannot add duplicate song");
+          console.log("[MusicPlayer] Cannot add duplicate song");
+          console.log('-------------------------------------------------------------------------');
           response.setColor(RED);
           response.setTitle("This song is already in the playlist.");
-          callback(response);
+          if(callback) callback(response);
           return;
         }
       }
+      If the connection is playing anything right now
       if(!this.connection.speaking){
-        console.log(`[MB] Playing: ${info.title}`);
         var dispatcher = this.connection.playStream(ytdl(url, YOUTUBE_OPTIONS),STREAM_OPTIONS);
-        /////////////////////////////////////////////////////////
         dispatcher.on('end',(reason)=>{
-          console.log("[MB.dispatcher] ended: " + reason);
+          console.log(`[MusicPlayer.Dispatcher] Finished: ${reason}`);
+          //If the reason is not null, try to play the next song
+          //(Only when the user gives a disconnect commant the reason is null)
+          if(reason != null || 'user'){
+            var nextSong = this.musicQueue.shift();
+            if(nextSong){
+              this.play(nextSong.url, new Discord.RichEmbed(), (res)=>{
+                this.textChannel.send(res);
+              });
+            }else{
+              this.disconnectVoiceChannel();
+              var msg = new Discord.RichEmbed().
+              setColor(GREEN)
+              .setTitle("Stopped")
+              .setDescription("Music queue is empty");
+              this.textChannel.send(msg);
+            }
+          }
         });
-        dispatcher.on('speaking', (result)=>{
-          console.log("[MB.dispatcher] speaking: " + result);
-        });
-        /////////////////////////////////////////////////////////
         this.nowPlaying = song;
+        console.log(`[MusicPlayer] Playing song: ${info.title}`);
+        console.log('-------------------------------------------------------------------------');
       }else{
-        console.log(`[MB] Added to queue: ${info.title}`);
+        console.log(`[MusicPlayer] Added song to queue: ${info.title}`);
+        console.log('-------------------------------------------------------------------------');
         this.musicQueue.push(song);
-        response.setDescription(`Added to queue: ${song.title}`);
-        var list = "";
+      }
+      response.setDescription(`Added to queue: ${song.title}`);
+      var list = "";
+      if(this.musicQueue.length >= 1){
         for(let i = 0; i < this.musicQueue.length;i++){
           list += `${i+1}: ${this.musicQueue[i].title} - ${this.musicQueue[i].artist}\n`;
         }
-        response.addField("**Queue**",list);
+      }else{
+        list = "Empty";
       }
+      response.addField("**Queue**",list);
       response.setDescription(`**Now Playing**\n${this.nowPlaying.title} - ${this.nowPlaying.artist}`);
       response.setThumbnail(this.nowPlaying.thumbnail);
       response.setColor(GREEN);
-      callback(response);
+      if(callback) callback(response);
     });
   }
+
+
 
 }
 module.exports = MusicPlayer;
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const MusicPlayer = require('./MusicPlayer');
-const musicPlayer = new MusicPlayer();
+const musicPlayer = new MusicPlayer(client);
 const BOT_TOKEN = process.env.BOT_TOKEN;
 //HEX COLORS
 const RED = 0xd10000;
@@ -13,6 +13,7 @@ const REQUIRED_PERMISSIONS = [
   'VIEW_CHANNEL', 'SEND_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS',
   'READ_MESSAGE_HISTORY', 'CONNECT', 'SPEAK', 'USE_VAD'
 ];
+
 /**
 -----------------------------------------------------------------------
 Connect the bot to the client
@@ -27,10 +28,8 @@ Confirm established connection
 client.on('ready', ()=>{
   console.log(`${client.user.tag} online!`);
   console.log('----------------------[Bot Servers]----------------------');
-  for (let item of client.guilds){
-    var id =  item[0];
-    var name = item[1].name;
-    console.log(`[${id}] ${name}`);
+  for (let guild of client.guilds){
+    console.log(`[${guild[0]}] ${guild[1]}`);
   }
   console.log('----------------------------------------------------------');
   console.log('[START] Listening....');
@@ -45,21 +44,20 @@ client.on('message',(message)=>{
   //Ignore bot messages & DM messages
   if(message.author.bot ||message.channel.type === 'dm') return;
   //If message is stop, or play, send to musicPlayer
+  console.log('-------------------------------------------------------------------------');
+  console.log('[CLIENT] Message Received:');
+  console.log(`   User: ${message.author.username}`);
+  console.log(`   Text: ${message.content}`);
   if(
     message.content === '!stop' ||
     /^(\!play )/i.exec(message.content)
   ){
     // -------- PRINT OUT MESSAGE ---------
-    console.log('[CLIENT] Message Received:');
-    console.log(` ${message.author.username}: ${message.content}`);
-    console.log('-------------------------------------------------------------------------');
     musicPlayer.command(message);
     return;
   }
-
   //Clear/Wipe history
   if(message.content === '!clear'){
-    console.log("[CLIENT] Clearing chat history...");
     var channel = message.channel;
     var response = new Discord.RichEmbed()
     .setTimestamp(message.createdAt)
@@ -67,10 +65,13 @@ client.on('message',(message)=>{
     var botUser = channel.members.find((val)=>val.id == client.user.id);
     var missingPermissions = botUser.missingPermissions(REQUIRED_PERMISSIONS);
     if(missingPermissions.length > 0){
-      console.log(`[CLIENT] Missing permissions: ${missingPermissions}`);
+      console.log(`[CLIENT] Can't clear chat- missing permissions: ${missingPermissions}`);
+      console.log('-------------------------------------------------------------------------');
       response.setColor(RED);
       response.setTitle(`${client.user.username} is missing permissions!`);
-      response.setDescription(`Go to https://discordapi.com/permissions.html to grant permissions.\n Enter \`415324652043239426\` as the Client ID`);
+      var text = "Go to https://discordapi.com/permissions.html to grant permissions.\n";
+      text += " Enter \`415324652043239426\` as the Client ID";
+      response.setDescription(text);
       response.addField(`Missing:`, `${missingPermissions}`);
       response.addField(`Required:`, `${REQUIRED_PERMISSIONS}\n`);
       channel.send(response);
@@ -132,16 +133,22 @@ client.on('message',(message)=>{
     var obj = musicPlayer.connection;
     if(obj){
       console.log(musicPlayer.connection.dispatcher.stream);
+      console.log('-------------------------------------------------------------------------');
       return;
     }else{
       console.log(null);
     }
   }
+  if(message.content === '!test5'){
+    musicPlayer.connection.dispatcher.end();
+  }
+
 
   function clearMessages(channel,response,callback){
     channel.fetchMessages({limit:100})
     .catch(error=>{
-      console.log('0 ' + error.message);
+      console.log('[CLIENT] Error1 while cleaning: ' + error.message);
+      console.log('-------------------------------------------------------------------------');
       response.setColor(RED);
       response.setDescription(error.message);
       callback(response);
@@ -149,14 +156,16 @@ client.on('message',(message)=>{
     })
     .then(messages => {
       if(messages.size === 0){
-        console.log('[CLIENT] Done clearing chat!');
+        console.log('[CLIENT] Cleared chat!');
+        console.log('-------------------------------------------------------------------------');
         response.setColor(GREEN);
         response.setDescription(`Cleaned up chat in **${channel.name}**`);
         callback(response);
         return;
       }
       channel.bulkDelete(messages,{filterOld:false}).catch(error=>{
-        console.log('1 ' + error.message);
+        console.log('[CLIENT] Error2 while cleaning: ' + error.message);
+        console.log('-------------------------------------------------------------------------');
         response.setColor(RED);
         response.setDescription(error.message);
         callback(response);
@@ -167,6 +176,8 @@ client.on('message',(message)=>{
       });
     });
   }
+});
 
-
+client.on('warn', (message)=>{
+  console.log('[CLIENT] Warning: ' + message);
 });
